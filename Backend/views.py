b8ed16d911d83ca98f4c3341bebbbd88dbe38abc
Saloni Sukirti <78682758+SaloniSukirti@users.noreply.github.com>
2024-06-25 23:05:09 +0530

    Add files via upload

---
+++ Backend/views.py
@@ -0,0 +1,25 @@
+# tasks/views.py
+
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
+
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT) # type: ignore
